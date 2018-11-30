b3157efad0364a323d55021d10061ad50c355b8b
Thomas Jansen <thomas@crissyfield.de>
2018-11-30 16:53:47 +0100

    Pull in Vue, Bulma, and FontAwesome for a simple page

---
+++ web/js/postfix-web-0.0.1.js
@@ -0,0 +1,13 @@
+var emailListApp = new Vue({
+  el: '#email-list-app',
+  data: {
+    // All data
+    foo: 'bar'
+  },
+  mounted() {
+    // TODO: Get initial setting
+  },
+  methods: {
+    // TODO: Manipulate data
+  }
+});
