d5c6a25cadf1c5e954e30eadbfa9c37f0f381489
Thomas Jansen <thomas@crissyfield.de>
2018-12-07 11:27:25 +0100

    Clean up JavaScript

--- web/js/postfix-web.js
+++ web/js/postfix-web.js
@@ -1,11 +1,10 @@
 var emailListApp = new Vue({
   el: '#email-app',
   data: {
-    // All data
+    // Model
     domains: []
   },
   mounted: function() {
-    // Initial load
     this.update()
   },
   methods: {
@@ -13,14 +12,56 @@ var emailListApp = new Vue({
     update: function() {
       var app = this
 
-      // Fetch from server
       axios
         .get('/api/domains/')
-        .then(function (response) { app.domains = decorateDomains(response.data) })
+        .then(function (response) { app.domains = app.decorateDomains(response.data) })
         .catch(function (error) { console.log(error) })
     },
 
-    // Check if new forwaerd is valid
+    // Create new forward
+    createForward: function(domain, event) {
+      var app = this
+
+      axios
+        .post('/api/domains/' + domain.name + '/forwards/', domain.create)
+        .then(function (response) { app.update() })
+        .catch(function (error) { console.log(error) })
+    },
+
+    // Update existing forward
+    updateForward: function(domain, forward, event) {
+      var app = this
+
+      axios
+        .put('/api/domains/' + domain.name + '/forwards/' + forward.from + '/', { to: forward.to } )
+        .then(function (response) { app.update() })
+        .catch(function (error) { console.log(error) })
+    },
+
+    // Delete existing forward
+    deleteForward: function(domain, forward, e) {
+      var app = this
+
+      axios
+        .delete('/api/domains/' + domain.name + '/forwards/' + forward.from + '/')
+        .then(function (response) { app.update() })
+        .catch(function (error) { console.log(error) })
+    },
+
+    // Decorate domains structure
+    decorateDomains: function(domains) {
+      _.forEach(domains, function(domain) {
+        // Keep original 'to' values
+        _.forEach(domain.forwards, function(forward) { forward.toOriginal = forward.to })
+
+        // Placeholder for new forward
+        domain.create = { from: '@' + domain.name, to: '' }
+      })
+
+      return domains
+    },
+
+    // Check if new forward is valid
     isNewForwardValid: function(domain) {
       // Invalid if 'from' or 'to' are empty
       if (domain.create.from.length == 0) { return false }
@@ -35,56 +76,6 @@ var emailListApp = new Vue({
       if (_.some(domain.forwards, predicate)) { return false }
 
       return true
-    },
-
-    // Create new forward
-    createForward: function(domain, event) {
-      var app = this
-
-      // Send to server
-      axios
-        .post('/api/domains/' + domain.name + '/forwards/', domain.create)
-        .then(function (response) { app.update() })
-        .catch(function (error) { console.log(error) })
-    },
-
-    // Update existing forward
-    updateForward: function(domain, forward, event) {
-      var app = this
-
-      // Send to server
-      axios
-        .put('/api/domains/' + domain.name + '/forwards/' + forward.from + '/', { to: forward.to } )
-        .then(function (response) { app.update() })
-        .catch(function (error) { console.log(error) })
-    },
-
-    // Delete existing forward
-    deleteForward: function(domain, forward, e) {
-      var app = this
-
-      // Send to server
-      axios
-        .delete('/api/domains/' + domain.name + '/forwards/' + forward.from + '/')
-        .then(function (response) { app.update() })
-        .catch(function (error) { console.log(error) })
     }
   }
 });
-
-function decorateDomains(domains) {
-  for (var i = 0; i < domains.length; i++) {
-    for (var j = 0; j < domains[i].forwards.length; j++) {
-      // Keep original 'to' value
-      domains[i].forwards[j].toOriginal = domains[i].forwards[j].to
-    }
-
-    // Placeholder for new forward
-    domains[i].create = {
-      from: '@' + domains[i].name,
-      to:   ''
-    }
-  }
-
-  return domains
-}
